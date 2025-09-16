00e789aca1a7ce926cbd671fa0773f485e61dce3
unknown <jslimit@gmail.com>
2025-09-16 16:01:16 +0900

    wk3

---
+++ src/mylib.py
@@ -0,0 +1,12 @@
+def sayHello():
+    print("hello")
+
+def getKey(keyPath):
+    d=dict()
+    f=open(keyPath,'r')
+    for line in f.readlines():
+        row=line.split('=')
+        row0=row[0]
+        d[row0]=row[1].strip()
+    f.close()
+    return d
